6b02d3aa8e19795911f55b35acb4864dc8887871
yagneshhihoriya <yagnesh@nimapinfotech.com>
2021-09-20 16:21:50 +0530

    first commit

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword = 'mongodb+srv://yagneshdhanani:r1gj0FzcTbu438Cf@userdetails.tsv9k.mongodb.net/file-uploads?retryWrites=true&w=majority';
+
+module.exports = {
+    mongoURI: dbPassword
+};
